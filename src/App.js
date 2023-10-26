ddd77972d63abb7a66c2d0aad94699a433c1c9dc
ecr00012 <ecr00012@mix.wvu.edu>
2023-10-26 19:35:35 -0400

    navbar and cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import cards from 'index.js'
+import 
 
 function App() {
   return (
     <div className="App">
+    navBar()
       <h1>CS 230L</h1>
       <h2>Section - 003</h2>
       <p>WVU ID: 800396984</p>
       <p>Hi, I am Creek</p>
+      
+      cards()
+
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css"
+import Card from 'react-bootstrap/Card'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +13,24 @@ root.render(
     <App />
   </React.StrictMode>
 );
+function navBar () {
+  return (
+    <nav class = "navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+  )
+
+}
+
+function cards(){
+  return (
+    <div>
+    <Card body>1</Card>
+    <Card body>2</Card>
+    <Card body>3</Card>
+    </div>
+  )
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
