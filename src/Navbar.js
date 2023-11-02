2261f9a7b280049e5cb5dcae128f0faed4488ecb
ecr00012 <ecr00012@mix.wvu.edu>
2023-11-02 16:15:00 -0400

    Navbar links functional, lab 7 finished

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,16 @@
 function navBar () {
     return (
       <nav class = "navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+         <a class="navbar-brand" href="#">Home</a>
+         <ul class = "navbar-nav">
+         <li class="nav-item active">
+        <a class = "nav-link" href = "Cards">About me</a>
+        </li>
+
+        <li class="nav-item active">
+        <a class=  "nav-link" href="Contact">Contact</a>
+        </li>
+        </ul>
       </nav>
     )
   
